b2915d9ae1f3391c3147fb9d9b6cbaaa2649a91f
losif63 <jadusuh@naver.com>
2024-01-25 07:31:03 +0000

    Added subdirectory structure

---
+++ Classes/Classes.cpp
@@ -0,0 +1,4 @@
+#include <torch/torch.h>
+#include "Classes.h"
+
+#define ASDF 1
--- vit.cpp
+++ vit.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <torch/torch.h>
 
 int main(void) {
     std::cout << "Hello World!!" << std::endl;
