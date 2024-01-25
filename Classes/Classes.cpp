ecdc2e3fa2fcbe62eee33e40ca4e624fcdc0ad69
losif63 <jadusuh@naver.com>
2024-01-25 08:28:51 +0000

    Starting to translate classes

--- Classes/Classes.cpp
+++ Classes/Classes.cpp
@@ -1,4 +1,26 @@
 #include <torch/torch.h>
 #include "Classes.h"
 
-#define ASDF 1
+struct FeedForwardImpl : torch::nn::Module {
+    FeedForwardImpl(int dim, int hidden_dim, float dropout = 0.0)
+        : net(
+            torch::nn::LayerNorm(torch::nn::LayerNormOptions({dim})),
+            torch::nn::Linear(torch::nn::LinearOptions(dim, hidden_dim)),
+            torch::nn::GELU(),
+            torch::nn::Dropout(torch::nn::DropoutOptions(dropout)),
+            torch::nn::Linear(torch::nn::LinearOptions(hidden_dim, dim)),
+            torch::nn::Dropout(torch::nn::DropoutOptions(dropout))
+        )
+    {
+        register_module("net", net);
+    }
+
+    torch::Tensor forward(torch::Tensor x) {
+        x = net->forward(x);
+        return x;
+    }
+
+    torch::nn::Sequential net;
+};
+TORCH_MODULE(FeedForward);
+
